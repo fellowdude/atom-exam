f6df99d0edd7629cadcb70bcc3adc30f892e295a
fellowdude <116744760+fellowdude@users.noreply.github.com>
2023-08-01 00:53:16 -0500

    POST | PUT: body validation

--- functions/src/controllers/Todo.ts
+++ functions/src/controllers/Todo.ts
@@ -1,46 +1,57 @@
 import { Request, Response } from 'express';
 import TodoRepository from '../repositories/Todo'
+import { ITodo } from '../interface/todo.interface'
 
+function isTodo(obj: any): obj is ITodo {
+    return 'title' in obj && 'description' in obj && 'completed' in obj;
+}
 export class TodoController {
     public create (req: Request, res: Response) {
-        TodoRepository.create(req.body)
-            .then(todo => { res.status(200).json(todo.id)})
-            .catch(err => {res.status(500).json(err)});
+        if(!isTodo(req.body)) res.status(500).json({detail: "Object is incorrect."});
+        else { 
+            TodoRepository.create({ create_date: new Date(), ...req.body})
+            .then((todo: any) => { res.status(200).json(todo.id)})
+            .catch((err: any) => {res.status(500).json(err)});
+        }
+        
     }
 
     public read (req: Request, res: Response) {
         TodoRepository.read()
-            .then(todo => {
+            .then((todo: any) => {
                 let results: Array<{id: string, data: FirebaseFirestore.DocumentData}> = []
-                todo.forEach(todo => results.push({ id: todo.id, data: todo.data() }))
+                todo.forEach((todo: any) => results.push({ id: todo.id, data: todo.data() }))
                 if (results.length) {
                     res.status(200).json(results)
                 } else {
                     res.status(404).json({detail: 'No records found'})
                 }
-            }).catch(err => {res.status(500).json(err)})
+            }).catch((err: any) => {res.status(500).json(err)})
     }
 
     public update (req: Request, res: Response) {
-        TodoRepository.update(req.params.id, req.body)
-            .then(todo => { res.status(201).json(todo)})
-            .catch(err => {res.status(500).json(err)});
+        if(!isTodo(req.body)) res.status(500).json({detail: "Object is incorrect."});
+        else { 
+            TodoRepository.update(req.params.id, { update_date: new Date(), ...req.body})
+                .then((todo: any) => { res.status(201).json(todo)})
+                .catch((err: any) => {res.status(500).json(err)});
+        }
     }
 
     public delete (req: Request, res: Response) { 
         TodoRepository.delete(req.params.id) 
             .then(() => { res.status(200).json({detail: `Todo deleted`}) })
-            .catch(err => {res.status(500).json(err)})
+            .catch((err: any) => {res.status(500).json(err)})
     }   
 
     public find (req: Request, res: Response) {
         TodoRepository.find(req.params.id)
-        .then(todo => {
+        .then((todo: any) => {
           if (todo.exists) {   
             res.status(200).json(todo.data())
           } else {
             res.status(404).json({detail: 'Not Found'})
           }
-        }).catch(err => {res.status(500).json(err)})
+        }).catch((err: any) => {res.status(500).json(err)})
     }
 }
---
+++ functions/src/interface/todo.interface.ts
@@ -0,0 +1,7 @@
+export interface ITodo{
+    title: string;
+    description: string;
+    completed: boolean;
+    create_date?: Date;
+    update_date?: Date;
+}
